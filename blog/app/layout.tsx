977d760f905eecff7a506c1e2f52e1b867a6ee1e
Lennox Mountain <lennoxmountain67@gmail.com>
2025-01-23 16:57:43 +0200

    removed boiler plate code

---
+++ blog/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import NavBar from "./components/navigation/navBar";
+
+export const metadata: Metadata = {
+  title: "Blog app",
+  description: "Hobby project",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className="mx-[90px]"
+      >
+        
+        {children}
+      </body>
+    </html>
+  );
+}
