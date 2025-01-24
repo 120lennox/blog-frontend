4420da067afb2a27593ea0345c637398885b6ad9
Lennox Mountain <lennoxmountain67@gmail.com>
2025-01-24 15:42:12 +0200

    styling cards

--- blog/app/components/recentPosts.tsx
+++ blog/app/components/recentPosts.tsx
@@ -2,29 +2,49 @@ import Image from "next/image"
 
 
 export default function RecentPost(){
-    return <div className="fixed">
+    return <div className="">
         <div>
-            <div>Recent blog posts</div>
-            <div>
+            <div className="text-[24px] font-semibold my-[32px]">Recent blog posts</div>
+            <div className="flex flex-row gap-4">
                 <div>
                     <div>
                         <Image
                             src="/Image_svg.svg"
                             alt="test"
-                            width={500}
-                            height={400}
+                            width={592}
+                            height={228}
                         />
                     </div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div>Sunday, 1 Jan 2025</div>
+                    <div>Ux review presentation</div>
+                    <div>How do you create compelling presentations that wow your colleagues and impress your managers?</div>
                     <div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
+                        <div>Design</div>
+                        <div>Research</div>
+                        <div>Presentation</div>
                     </div>
                 </div>
-                <div></div>
+                <div>
+                    <div>
+                        <div>
+                            <Image 
+                                src="/card_svg.svg"
+                                alt="test 2"
+                                width={320}
+                                height={200}
+                            />
+                        </div>
+                        <div>
+                            <div>Sunday, 1 Jan 2025</div>
+                            <div>Migrating to Linear 101</div>
+                            <div>Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...</div>
+                            <div>
+                                <div>Design</div>
+                                <div>Research</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -1,12 +1,14 @@
 import RecentPost from "./components/recentPosts";
 
 export default function Home() {
-  return <div>
-        <div className="fixed my-[50px]">
-          <div className="w-full h-[295] overflow-none border border-r-[#090D1F] border-l-[#090D1F] flex items-center">
+  return <div className="flex flex-col">
+        <div className="my-[50px]">
+          <div className="w-full h-[295] overflow-hidden border border-r-[#090D1F] border-l-[#090D1F] flex items-center">
             <div className=" text-[219.9px] font-bold">THE BLOG</div>
           </div>
         </div>
-        <RecentPost />
+        <div>
+          <RecentPost />
+        </div>
   </div>
 }
