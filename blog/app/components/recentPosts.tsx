f38af4a6a7bcf492a8de1597b19a4a7cd8a8fd57
Lennox Mountain <lennoxmountain67@gmail.com>
2025-01-24 14:21:38 +0200

    mind messed up cant concentrate

--- blog/app/components/recentPosts.tsx
+++ blog/app/components/recentPosts.tsx
@@ -1,5 +1,31 @@
+import Image from "next/image"
+
+
 export default function RecentPost(){
-    return <div>
-        
+    return <div className="fixed">
+        <div>
+            <div>Recent blog posts</div>
+            <div>
+                <div>
+                    <div>
+                        <Image
+                            src="/Image_svg.svg"
+                            alt="test"
+                            width={500}
+                            height={400}
+                        />
+                    </div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+                <div></div>
+            </div>
+        </div>
     </div>
 }
--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -1,9 +1,12 @@
+import RecentPost from "./components/recentPosts";
+
 export default function Home() {
   return <div>
-        <div className="my-[50px]">
+        <div className="fixed my-[50px]">
           <div className="w-full h-[295] overflow-none border border-r-[#090D1F] border-l-[#090D1F] flex items-center">
             <div className=" text-[219.9px] font-bold">THE BLOG</div>
           </div>
         </div>
+        <RecentPost />
   </div>
 }
