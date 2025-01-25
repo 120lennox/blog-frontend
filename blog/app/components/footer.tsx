f6991c71b8a98bb0fbc1292f76f5b548ff4efb0f
Lennox Mountain <lennoxmountain67@gmail.com>
2025-01-25 16:41:49 +0200

    finally styled footer

--- blog/app/components/footer.tsx
+++ blog/app/components/footer.tsx
@@ -1,9 +1,18 @@
+import { github, twitter } from "./navigation/icons";
+
 export default function Footer(){
-    return <div>
-        <div>
-            <div><span>&copy;</span>2025 nthano.systems</div>
+    return <div className="my-4 text-slate-300">
+        <div className="flex flex-row space-x-10 items-center">
+            <div className="flex flex-row space-x-2"><span>&copy;</span><div>2025 nthano.systems</div></div>
             <div>Cooked in Malawi</div>
-            <div></div>
+            <div className="flex flex-row space-x-1 items-center">
+                {github.icon}
+                <div>120lennox</div>
+            </div>
+            <div className="flex flex-row items-center space-x-1">
+                {twitter.icon}
+                <div>@mountain_lennox</div>
+            </div>
         </div>
     </div>
 }
